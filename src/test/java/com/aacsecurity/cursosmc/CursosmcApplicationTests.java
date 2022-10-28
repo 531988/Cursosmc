31aaa564c4300bc2eee6b2b95709d7958e4cd132
Alexsandro Costa <alexs.alexsandrocosta@hotmail.com>
2022-10-28 10:16:28 -0300

    Rest Testado

---
+++ src/test/java/com/aacsecurity/cursosmc/CursosmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacsecurity.cursosmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
